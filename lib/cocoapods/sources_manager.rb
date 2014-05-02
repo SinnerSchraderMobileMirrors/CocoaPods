a67e8db693c13731e77191d76abf11ddd9de5a45
Kyle Fuller <inbox@kylefuller.co.uk>
2014-05-02 21:16:12 +0100

    [Sources] Use `git pull` with the `--ff-only` flag
    This fixes issue where since we were using `--no-commit`, git was
    starting a merge but not committing it. Then subsequent `git pull`'s would
    result in the following error from git:
    
    > You have not concluded your merge (MERGE_HEAD exists).
    > Please, commit your changes before you can merge.
    
    This commit will mean that `--ff-only` is passed to `git pull`  which means
    it wont rebase or merge. It will only allow a fast forward to progress
    with the pull.
    
    Fixes #2024

--- lib/cocoapods/sources_manager.rb
+++ lib/cocoapods/sources_manager.rb
@@ -152,7 +152,7 @@ module Pod
         sources.each do |source|
           UI.section "Updating spec repo `#{source.name}`" do
             Dir.chdir(source.data_provider.repo) do
-              output = git!("pull --no-rebase --no-commit")
+              output = git!("pull --ff-only")
               UI.puts output if show_output && !config.verbose?
             end
             check_version_information(source.data_provider.repo)
